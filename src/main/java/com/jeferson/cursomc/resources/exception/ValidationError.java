bbcfeb34fb9e2d96d9f1b235065727a8cc10bc4e
jefsantos <jsantos@apoioprodesp.sp.gov.br>
2018-12-28 14:28:15 -0200

    Personalizando Retorno da Validação - REVISAR

---
+++ src/main/java/com/jeferson/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,28 @@
+package com.jeferson.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+
+
+	private static final long serialVersionUID = 1L;
+
+	private List <FieldMessage>erros = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErros() {
+		return erros;
+	}
+
+	public void addError(String fieldName, String messagem) {
+		
+		erros.add(new FieldMessage(fieldName, messagem ));
+		
+	}
+	
+	
+}
