8ffa32f4da03a26d50904182b37ee3392f293df1
jefsantos <jsantos@apoioprodesp.sp.gov.br>
2019-02-01 17:05:12 -0200

    AULA 53 Criando perfil de PROD E DEV NO BANCO

---
+++ src/main/java/com/jeferson/cursomc/config/prodConfig.java
@@ -0,0 +1,24 @@
+package com.jeferson.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.jeferson.cursomc.services.DBService;
+
+@Configuration
+@Profile("prod")
+public class prodConfig {
+	
+	@Autowired
+	private DBService dbservice;
+	
+@Bean
+public boolean instantiateDatabase() throws ParseException {
+	dbservice.instantiateTestDatabase();
+	return true;
+}
+}
