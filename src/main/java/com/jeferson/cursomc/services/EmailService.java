e9ba85813819ff04064fb58f391738c2d8b21860
Jeferson Alves <jeh.uninove@gmail.com>
2019-03-26 02:17:48 -0300

    Envio do Projeto para PRODUÇÃO AULA 53 VOLTANDO

--- src/main/java/com/jeferson/cursomc/services/EmailService.java
+++ src/main/java/com/jeferson/cursomc/services/EmailService.java
@@ -3,9 +3,11 @@ package com.jeferson.cursomc.services;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
 import com.jeferson.cursomc.domain.Pedido;
 
+@Service
 public interface EmailService {
 
 	
